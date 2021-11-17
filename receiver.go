0dccac5e5fe0c8fd8051901ced6a01d1ae458736
Kitex101 <sugandha.amatya@gmail.com>
2021-11-17 08:07:06 +0545

    csv output of alarms

--- receiver.go
+++ receiver.go
@@ -12,7 +12,8 @@ set GOOS=linux
 go build
 */
 import (
-	"encoding/json"
+	"bytes"
+	"encoding/csv"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -49,6 +50,27 @@ type (
 		StartsAt    string            `json:"startsAt,omitempty"`
 		EndsAt      string            `json:"EndsAt,omitempty"`
 	}
+
+	FMS struct {
+		Identifier    string `json:"identifier"`
+		Node          string `json:"node"`
+		Suggestion    string `json:"suggestion"`
+		AlarmName     string `json:"alarmname"`
+		AlertGroup    string `json:"alertgroup"`
+		Summary       string `json:"summary"`
+		AlertKey      string `json:"alertkey"`
+		Type          string `json:"type"` //1 and 2
+		Severity      string `json:"severity"`
+		Domain        string `json:"domain"`
+		Manager       string `json:"manager"`
+		AlarmPriority string `json:"alarmpriority"`
+		RawSeverity   string `json:"rawseverity"`
+	}
+
+	NMS struct {
+		Type   string  `json:"type"` //1 and 2
+		Alerts []Alert `json:"alerts"`
+	}
 )
 
 func main() {
@@ -76,18 +98,73 @@ func main() {
 	// GET /api/register
 	app.Post("/sendMetrics", func(c *fiber.Ctx) error {
 		var data HookMessage
+		var fms FMS
+		var nms NMS
 
 		if err := c.BodyParser(&data); err != nil {
 			Info.Println(err)
 			return err
 		}
+		//log.Println(data.Status)
+		//log.Println(data)
+		//log.Println(data)
+		//log.Println(data.Alerts[0])
 
-		json, _ := json.MarshalIndent(data, "", " ")
 		//fmt.Println(reflect.TypeOf(json))
 		filePath := fmt.Sprintf("%s%s%d", path, "/", time.Now().UnixNano())
-		//fmt.Println("path:", filePath)
 
-		_ = ioutil.WriteFile(filePath, json, 0644)
+		//data_json, _ := json.MarshalIndent(data, "", " ")
+		//_ = ioutil.WriteFile("./logs", data_json, 0644)
+		//fmt.Println("path:", filePath)
+		fms.Identifier = data.Status
+
+		fms.Identifier = data.Alerts[0].Labels["alertname"] + data.Alerts[0].Labels["function"] + data.Alerts[0].Labels["hostname"] + data.Alerts[0].Labels["type"]
+		fms.Node = data.Alerts[0].Labels["hostname"] + "-" + data.Alerts[0].Labels["instance"] + "-" + data.Alerts[0].Labels["function"]
+		fms.AlarmName = data.Alerts[0].Labels["alertname"]
+		fms.AlertGroup = data.Alerts[0].Labels["alertgroup"]
+		fms.Summary = data.Alerts[0].Annotations["summary"]
+		fms.Suggestion = data.Alerts[0].Annotations["suggestion"]
+		fms.AlertKey = data.Alerts[0].Labels["function"]
+		fms.Type = data.Status
+		fms.Severity = data.Alerts[0].Labels["severity"]
+		fms.Domain = data.Alerts[0].Labels["job"]
+		fms.Manager = data.Alerts[0].Labels["devicetype"]
+		fms.AlarmPriority = data.Alerts[0].Labels["alarmpriority"]
+
+		pairs := [][]string{}
+		pairs = append(pairs, []string{"identifier", "node", "alarmname", "alertgroup", "summary", "suggestion", "alertKey", "type", "severity", "domain", "manager", "alarmPriority"})
+		pairs = append(pairs, []string{fms.Identifier, fms.Node, fms.AlarmName, fms.AlertGroup, fms.Summary, fms.Suggestion, fms.AlertKey, fms.Type, fms.Severity, fms.Domain, fms.Manager, fms.AlarmPriority})
+
+		b := new(bytes.Buffer)
+		w := csv.NewWriter(b)
+
+		w.WriteAll(pairs)
+
+		if err := w.Error(); err != nil {
+			log.Fatal(err)
+		}
+
+		csvString := b.String()
+
+		log.Println(csvString)
+
+		//log.Println(fms.Node)
+		nms.Alerts = data.Alerts
+		nms.Type = data.Status
+
+		// How to get value from map in golang
+		//alertname, exists := data.CommonLabels["alertname"]
+		//fmt.Printf("key exists in map: %t, value: %v \n", exists, alertname)
+		/*
+			nms_json, err := json.MarshalIndent(nms, "", " ")
+			if err != nil {
+				fmt.Println(err)
+			}
+			log.Println(nms)
+			log.Println(nms_json)
+		*/
+
+		_ = ioutil.WriteFile(filePath, []byte(csvString), 0644)
 		/*
 			file, err := os.OpenFile(strconv.FormatInt(time.Now().UnixNano(), 10), os.O_APPEND|os.O_WRONLY, 0644)
 			if err != nil {
@@ -105,6 +182,5 @@ func main() {
 		//Info.Println(data)
 		return c.SendStatus(fiber.StatusOK)
 	})
-
 	log.Fatal(app.Listen(":5001"))
 }
